746b4c0c53c2646f06c4714158ba0a4be2d2129e
paullin <pengpenglin@163.com>
2026-04-04 22:15:25 +0800

    【子目录及详情页生成】完成迭代1-历史代码清理与依赖配置

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,36 +1,5 @@
-import { defineCollection, z, reference } from "astro:content";
-import { file, glob } from "astro/loaders";
-
-const blogCollection = defineCollection({
-    loader: glob({
-        pattern: "**/*.{md,mdx}",
-        base: "./src/content/blog",
-        generateId: ({ entry }) => {
-            return entry.replace(/\.(md|mdx)$/, "");
-        },
-    }),
-    schema: ({ image }) =>
-        z.object({
-            title: z.string(),
-            description: z.string(),
-            author: reference("authors"),
-            pubDate: z.date(),
-            isDraft: z.boolean(),
-            linkedContent: z.string().optional(),
-            image: image(),
-            imageAlt: z.string().optional(),
-        }),
-});
-
-const authors = defineCollection({
-    loader: file("src/content/authors/authors.json"),
-    schema: ({ image }) =>
-        z.object({
-            name: z.string(),
-            image: image(),
-            position: z.record(z.string()),
-        }),
-});
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const homepage = defineCollection({
     loader: glob({
@@ -150,7 +119,5 @@ const homepage = defineCollection({
 });
 
 export const collections = {
-    blog: blogCollection,
-    authors: authors,
     homepage: homepage,
 };
